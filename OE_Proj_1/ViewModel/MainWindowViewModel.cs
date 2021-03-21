1aca714445acf9ec22d3f99db8c51c7ce3d7332f
Karol Patko <karolpatko@gmail.com>
2021-03-21 13:55:27 +0100

    Add f

--- OE_Proj_1/ViewModel/MainWindowViewModel.cs
+++ OE_Proj_1/ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System;
 
 namespace OE_Proj_1.ViewModel
 {
@@ -35,9 +36,25 @@ namespace OE_Proj_1.ViewModel
                     }
                 }*/
 
-        public double f()
+        
+
+        public double f(double x, double y)
         {
-            return 0.0;
+            //MIN f(x,y)=-1,9133 (x, y)=(-0.54719, -0.54719)
+            return Math.Sin(x + y) + (x - y)*(x - y) - 1.5*x+2.5*y + 1;
+        }
+
+        public void doSelection()
+        {
+            switch (selection)
+            {
+                case "BEST":
+                    return;
+                case "ROULETTE":
+                    return;
+                case "TOURNAMENT":
+                    return;
+            }
         }
     }
 }
